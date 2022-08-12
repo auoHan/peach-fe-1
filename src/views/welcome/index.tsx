48e721df1b0d6d87009d59b64de2a54e967f2828
h <710543268@qq.com>
2022-08-12 08:51:24 +0000

    welcome-css初始化

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,9 +1,19 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+import logo from '@/assets/icons/peach.svg'
+import s from '@/views/welcome/welcome.module.scss'
 export const Welcome = defineComponent({
- setup: (props, context) => {
-  return () => (
-   <div><RouterView/></div>
-  )
- }
+	setup: (props, context) => {
+		return () => (
+			<div class={s.wrapper}>
+				<header>
+					<img src={logo} />
+					<h1>蜜桃记账</h1>
+				</header>
+				<div>
+					<RouterView />
+				</div>
+			</div>
+		)
+	}
 })
