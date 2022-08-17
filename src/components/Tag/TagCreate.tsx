5633bf3ea00fe7d9881e1f28db26fb98aed175d9
h <710543268@qq.com>
2022-08-18 00:59:45 +0800

    完成创建标签表单验证功能

--- src/components/Tag/TagCreate.tsx
+++ src/components/Tag/TagCreate.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/shared/Button/Button'
 import { EmojiSelect } from '@/shared/EmojiList/EmojiSelect'
 import { Icon } from '@/shared/Icon/Icon'
 import s from './TagCreate.module.scss'
+import { Rules, validate } from '@/shared/Validate/Validate'
 export const TagCreate = defineComponent({
 	props: {
 		name: {
@@ -13,15 +14,34 @@ export const TagCreate = defineComponent({
 	setup: (props, context) => {
 		const formData = reactive({
 			name: '',
-			sign: 'x'
+			sign: ''
 		})
+		const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
+		const onSubmit = (e: Event) => {
+			const rules: Rules<typeof formData> = [
+				{ key: 'name', type: 'required', message: '必填' },
+				{
+					key: 'name',
+					type: 'pattern',
+					regex: /^.{1,4}$/,
+					message: '只能填 1 到 4 个字符'
+				},
+				{ key: 'sign', type: 'required', message: '必填' }
+			]
+			Object.assign(errors, {
+				name: undefined,
+				sign: undefined
+			})
+			Object.assign(errors, validate(formData, rules))
+			e.preventDefault()
+		}
 		return () => (
 			<MainLayout>
 				{{
 					title: () => '新建标签',
 					icon: () => <Icon name="left" onClick={() => {}} />,
 					default: () => (
-						<form class={s.form}>
+						<form class={s.form} onSubmit={onSubmit}>
 							<div class={s.formRow}>
 								<label class={s.formLabel}>
 									<span class={s.formItem_name}>标签名</span>
@@ -32,7 +52,7 @@ export const TagCreate = defineComponent({
 										></input>
 									</div>
 									<div class={s.formItem_errorHint}>
-										<span>必填</span>
+										<span>{errors['name'] ? errors['name'][0] : '　'}</span>
 									</div>
 								</label>
 							</div>
@@ -46,7 +66,7 @@ export const TagCreate = defineComponent({
 										/>
 									</div>
 									<div class={s.formItem_errorHint}>
-										<span>必填</span>
+										<span>{errors['sign'] ? errors['sign'][0] : '　'}</span>
 									</div>
 								</label>
 							</div>
