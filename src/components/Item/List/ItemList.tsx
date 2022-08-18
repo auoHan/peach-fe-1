2d831bea724fb7e8ec0f1d10f91d2612be582fd9
h <710543268@qq.com>
2022-08-18 22:43:27 +0800

    每次点击自定义时间都弹出弹出框

--- src/components/Item/List/ItemList.tsx
+++ src/components/Item/List/ItemList.tsx
@@ -35,11 +35,6 @@ export const ItemList = defineComponent({
 				end: time.lastDayOfYear()
 			}
 		]
-		watchEffect(() => {
-			if (refSelected.value === '自定义时间') {
-				refOverlayVisible.value = true
-			}
-		})
 		const refOverlayVisible = ref(false)
 		const onSubmitCustomTime = (e: Event) => {
 			e.preventDefault()
@@ -55,6 +50,7 @@ export const ItemList = defineComponent({
 							<Tabs
 								classPrefix={'customTabs'}
 								v-model:selected={refSelected.value}
+								onUpdate:selected={() => (refOverlayVisible.value = true)}
 							>
 								<Tab name="本月">
 									<ItemSummary
