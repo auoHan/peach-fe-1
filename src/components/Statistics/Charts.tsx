af5b0baae228d68e9b8bbda9bc1ecf8a28125bef
h <710543268@qq.com>
2022-08-29 15:21:25 +0800

    切换 kind 时重新请求数据

--- src/components/Statistics/Charts.tsx
+++ src/components/Statistics/Charts.tsx
@@ -1,5 +1,5 @@
 import { FormItem } from '@/shared/Form/Form'
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue'
+import { computed, defineComponent, onMounted, PropType, ref, watch } from 'vue'
 import s from './Charts.module.scss'
 import { LineChart } from './LineChart'
 import { PieChart } from './PieChart'
@@ -46,7 +46,7 @@ export const Charts = defineComponent({
 			})
 		})
 
-		onMounted(async () => {
+		const fetchData1 = async () => {
 			const response = await http.get<{ groups: Data1; summary: number }>(
 				'/items/summary',
 				{
@@ -60,7 +60,9 @@ export const Charts = defineComponent({
 			console.log('response.data')
 			console.log(response.data)
 			data1.value = response.data.groups
-		})
+		}
+		onMounted(fetchData1)
+		watch(() => kind.value, fetchData1)
 		// data2
 
 		const data2 = ref<Data2>([])
@@ -79,7 +81,7 @@ export const Charts = defineComponent({
 				percent: Math.round((item.amount / total) * 100)
 			}))
 		})
-		onMounted(async () => {
+		const fetchData2 = async () => {
 			const response = await http.get<{ groups: Data2; summary: number }>(
 				'/items/summary',
 				{
@@ -91,7 +93,9 @@ export const Charts = defineComponent({
 				}
 			)
 			data2.value = response.data.groups
-		})
+		}
+		onMounted(fetchData2)
+		watch(() => kind.value, fetchData2)
 		return () => (
 			<div class={s.wrapper}>
 				<FormItem
